36bdd6a3c4b3ad7e381ed4ae73e5c1b8cc9900eb
Angelo Totanes <angelot64@gmail.com>
2019-11-25 16:15:18 -0800

    Added user form and user display page for debugging purposes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { MessagesComponent } from './messages/messages.component';
+import { InputUserDataFormComponent } from './input-user-data-form/input-user-data-form.component';
+import { DisplayUserDataComponent } from './display-user-data/display-user-data.component';
 
 
 const routes: Routes = [
+  { path: '', component: InputUserDataFormComponent },
+  { path: 'userinfo', component: DisplayUserDataComponent },
   { path: 'homepage', component: HomepageComponent },
   { path: 'messages', component: MessagesComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { MessagesComponent } from './messages/messages.component';
+import { InputUserDataFormComponent } from './input-user-data-form/input-user-data-form.component';
+import { DisplayUserDataComponent } from './display-user-data/display-user-data.component';
 
 @NgModule({
   declarations: [
@@ -16,12 +18,15 @@ import { MessagesComponent } from './messages/messages.component';
     NavbarComponent,
     FooterComponent,
     HomepageComponent,
-    MessagesComponent
+    MessagesComponent,
+    InputUserDataFormComponent,
+    DisplayUserDataComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
